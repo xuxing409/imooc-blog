0e56da9447bcd2cb6d2daebcc18a0a776417e33c
Snxy <892573319@qq.com>
2023-01-03 01:34:10 +0800

    feat: 新增搜索框组件、mytabs组件、封装网络请求模块

--- pages/hot/hot.vue
+++ pages/hot/hot.vue
@@ -1,19 +1,50 @@
 <template>
-	<view>
-		
-	</view>
+  <view class="hot-container">
+    <!-- logo -->
+    <image class="logo" mode="aspectFit" src="@/static/images/logo.png" />
+
+    <view class="search-box" @click="onToSearch">
+      <!-- 搜索模块 -->
+      <my-search placeholderText="uni-app 自定义组件" />
+    </view>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			};
-		}
-	}
+import { getHotTabs, getHotListFromTabType } from "api/hot";
+export default {
+  data() {
+    return {
+      // tabs 数据源
+      tabData: [],
+      // 当前的切换 index
+      currentIndex: 0,
+      // list 列表数据加载过程
+      isLoading: true,
+    };
+  },
+  // 在实例创建完成后被立即调用
+  created() {
+    this.getHotTabs();
+  },
+  methods: {
+    async getHotTabs() {
+      // uniapp 支持 async await
+      const { data: res } = await getHotTabs();
+      this.tabData = res.list;
+      // 获取列表数据
+      this.getHotListFromTab();
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-
+.hot-container {
+  background-color: $uni-bg-color;
+  .logo {
+    width: 100%;
+    height: 80px;
+  }
+}
 </style>
