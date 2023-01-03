96970e55a6231758c4d4da0d80b15695782f30e5
Snxy <892573319@qq.com>
2023-01-04 02:32:52 +0800

    mytabs组件渲染

--- pages/hot/hot.vue
+++ pages/hot/hot.vue
@@ -6,6 +6,12 @@
     <view class="search-box" @click="onToSearch">
       <!-- 搜索模块 -->
       <my-search placeholderText="uni-app 自定义组件" />
+      <my-tabs
+        :tabData="tabData"
+        :defaultIndex="currentIndex"
+        :config="{ textColor: '#333333' }"
+        @tabClick="tabClick"
+      ></my-tabs>
     </view>
   </view>
 </template>
@@ -21,6 +27,14 @@ export default {
       currentIndex: 0,
       // list 列表数据加载过程
       isLoading: true,
+      // 以 index 为 key，对应的 list 为 val
+      listData: {},
+      // 当前 swiper 的高度
+      currentSwiperHeight: 0,
+      // 以 index 为 key，对应的 swiper 的高度 为 val
+      swiperHeightData: {},
+      // 当前的滚动距离
+      currentPageScrollTop: 0,
     };
   },
   // 在实例创建完成后被立即调用
@@ -33,7 +47,29 @@ export default {
       const { data: res } = await getHotTabs();
       this.tabData = res.list;
       // 获取列表数据
-      this.getHotListFromTab();
+      // this.getHotListFromTab();
+    },
+
+    /**
+     * list 列表数据
+     */
+    async getHotListFromTab() {
+      // 展示 loading
+      this.isLoading = true;
+      // 获取列表数据
+      const id = this.tabData[this.currentIndex].id;
+      const { data: res } = await getHotListFromTabType(id);
+      // 放入数据缓存
+      this.listData[this.currentIndex] = res.list;
+      // 隐藏 loading
+      this.isLoading = false;
+      // 因为 this.$nextTick 存在一定的兼容性问题，所以更加推荐使用 setTimeout
+      setTimeout(async () => {
+        // 获取当前 swiper 的高度
+        this.currentSwiperHeight = await this.getCurrentSwiperHeight();
+        // 放入缓存
+        this.swiperHeightData[this.currentIndex] = this.currentSwiperHeight;
+      }, 0);
     },
   },
 };
--- utils/request.js
+++ utils/request.js
@@ -1,13 +1,20 @@
+import md5 from "md5";
 const BASE_URL = "https://api.imooc-blog.lgdsunday.club/api";
 
 function request({ url, data, method }) {
+  // 添加验证
+  let e = parseInt(Date.now() / 1e3),
+    n = e + "LGD_Sunday-1991-uni";
+
   return new Promise((resolve, reject) => {
     uni.request({
       url: BASE_URL + url,
       data,
       method,
       header: {
-        Authorization: store.state.user.token,
+        // Authorization: store.state.user.token,
+        icode: md5(n),
+        codeType: e,
       },
       success: ({ data }) => {
         if (data.success) {
